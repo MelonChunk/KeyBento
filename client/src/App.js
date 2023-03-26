fd4199cb638c4a28a620de3cdaeabb48d7a6fbda
Arvid J. Kingl <arvid@plutodata.co.uk>
2023-03-26 22:56:53 +0100

    added some refactor of the code

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,15 @@
+import Container from 'react-bootstrap/Container';
+import Header from './components/Header';
+import TopContent from './components/TopContent';
+import Body from './components/Body';
+
 export default function App() {
-
-  const user = {
-    id: 1,
-    first_name: 'Melon',
-    last_name: 'Chunk'
-  }
-
-  const destinations = [{id:1, location:'London'}, {id:2, location:'New York'}, {id:3, location:'Berlin'}];
-
   return (
-    <>
-    <h1>Key Bento</h1>
-    <p>Hello {user.first_name} {user.last_name}, good to see you back!</p>
-    <p>The following destinations are available: </p>
-      {destinations.length === 0 ?
-          <p> Unfortunately, no destinations are currently available </p>
-        :
-        <ul>
-          {destinations.map(x => {return (
-              <p key={x.id}>
-              <li>{x.location}</li>
-              </p>
-            )})}
-        </ul>
-      }
-    </>
-  );
+    <Container fluid className="App">
+    <Header />
+    <Body sidebar>
+      <TopContent />
+    </Body>
+  </Container>
+  )
 }
