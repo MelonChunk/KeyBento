705986ff7fe08b261701b0a3a37ece8f354eb077
Arvid J. Kingl <arvid@plutodata.co.uk>
2023-03-27 22:15:36 +0100

    added basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import Container from 'react-bootstrap/Container';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header';
-import TopContent from './components/TopContent';
-import Body from './components/Body';
+import TopContent from './pages/TopContent';
+import ExplorePage from './pages/ExplorePage';
+import LoginPage from './pages/LoginPage';
+import UserPage from './pages/UserPage';
 
 export default function App() {
   return (
     <Container fluid className="App">
-    <Header />
-    <Body sidebar>
-      <TopContent />
-    </Body>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<TopContent />} />
+          <Route path="/explore" element={<ExplorePage/>}/>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/user/:username" element={<UserPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
   </Container>
   )
 }
