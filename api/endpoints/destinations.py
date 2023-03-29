c445b53843734458209921f9273a9e8aa9b6a333
Arvid J. Kingl <arvid@plutodata.co.uk>
2023-03-29 17:11:46 +0100

    added sample end point

---
+++ api/endpoints/destinations.py
@@ -0,0 +1,19 @@
+from typing import List
+from fastapi import APIRouter, Request
+
+from schemas.destinations import Destination
+
+router = APIRouter()
+
+
+@router.get("/api/destinations", response_model=List[Destination])
+async def game_getter(
+    request: Request,
+):  # pylint: disable=unused-argument
+
+    destinations = [
+        Destination(id=1, location="London"),
+        Destination(id=2, location="New York"),
+        Destination(id=3, location="Berlin"),
+    ]
+    return destinations
--- client/src/pages/TopContent.js
+++ client/src/pages/TopContent.js
@@ -1,6 +1,8 @@
 import Stack from 'react-bootstrap/Stack';
 import Container from 'react-bootstrap/Container';
 import Body from '../components/Body';
+import { useState } from 'react';
+import Spinner from 'react-bootstrap/Spinner';
 
 export default function TopContent(){
   const user = {
@@ -8,8 +10,8 @@ export default function TopContent(){
     first_name: 'Melon',
     last_name: 'Chunk'
   }
-
-  const destinations = [{id:1, location:'London'}, {id:2, location:'New York'}, {id:3, location:'Berlin'}];
+  const [destinations, setDestinations] = useState()
+  // const destinations = [{id:1, location:'London'}, {id:2, location:'New York'}, {id:3, location:'Berlin'}];
 
   return (<Body sidebar>
             <Stack>
@@ -17,6 +19,10 @@ export default function TopContent(){
                 <p>Hello {user.first_name} {user.last_name}, good to see you back!</p>
               </Container>
               <Container>
+                {destinations === undefined ?
+                    <Spinner animation="border"/>
+                :
+                <>
                 <p>The following destinations are available: </p>
                   {destinations.length === 0 ?
                       <p> Unfortunately, no destinations are currently available </p>
@@ -29,6 +35,9 @@ export default function TopContent(){
                         )})}
                     </ul>
                   }
+                }
+                </>
+              }
                 </Container>
              </Stack>
            </Body>
