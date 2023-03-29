2a99cbf25ff91d75dbee5f61bbef284dd35a8a47
Arvid J. Kingl <arvid@plutodata.co.uk>
2023-03-29 23:11:38 +0100

    added mock db generator back for testing

--- src/api/core/db.py
+++ src/api/core/db.py
@@ -1,6 +1,9 @@
+import tempfile
 import sqlalchemy as sa
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base import Base
+
 
 TEST_URL = "sqlite:///./test.db"
 
@@ -9,3 +12,13 @@ TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=test_
 # Set up the database once
 Base.metadata.drop_all(bind=test_engine)
 Base.metadata.create_all(bind=test_engine)
+
+
+def mock_db_session():
+    """Used purely for unit testing"""
+    path = tempfile.mkdtemp("mock")
+    db_url = f"sqlite:///{path}/db.sqlite"
+    engine = create_engine(db_url, connect_args={"check_same_thread": False})
+    Base.metadata.create_all(engine)
+    yield scoped_session(sessionmaker(bind=engine))
+    engine.dispose()
